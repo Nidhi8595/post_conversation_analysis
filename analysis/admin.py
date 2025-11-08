aca61df3136121065ba60847e5cec494110008b9
Neelakshi <neelakshikadyan@gmail.com>
2025-11-08 20:47:26 +0530

    Register models and relationships in Django admin

--- analysis/admin.py
+++ analysis/admin.py
@@ -1,3 +1,18 @@
+# Registering models for admin interface
 from django.contrib import admin
+from .models import Conversation, Message, ConversationAnalysis
 
-# Register your models here.
+class MessageInline(admin.TabularInline):
+    model = Message
+    extra = 0
+
+class ConversationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at')
+    inlines = [MessageInline]
+
+class ConversationAnalysisAdmin(admin.ModelAdmin):
+    list_display = ('conversation', 'clarity_score', 'relevance_score', 'overall_score', 'created_at')
+
+admin.site.register(Conversation, ConversationAdmin)
+admin.site.register(Message)
+admin.site.register(ConversationAnalysis, ConversationAnalysisAdmin)
--- analysis/models.py
+++ analysis/models.py
@@ -2,8 +2,6 @@ from django.db import models
 
 # Models
 
-from django.db import models
-
 class Conversation(models.Model):
     title = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
