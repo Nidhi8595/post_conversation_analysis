68b23a85537398f3a042569619ef35f9d6e5a49d
Neelakshi <neelakshikadyan@gmail.com>
2025-11-11 10:38:01 +0530

    Add Django REST Framework and serializers for models

---
+++ analysis/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import Conversation, Message, ConversationAnalysis
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = ['id', 'sender', 'text']
+
+class ConversationSerializer(serializers.ModelSerializer):
+    messages = MessageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Conversation
+        fields = ['id', 'title', 'created_at', 'messages']
+
+class ConversationAnalysisSerializer(serializers.ModelSerializer):
+    conversation = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = ConversationAnalysis
+        fields = [
+            'conversation', 'clarity_score', 'relevance_score', 'accuracy_score',
+            'completeness_score', 'sentiment', 'empathy_score', 'response_time',
+            'resolution', 'escalation_needed', 'fallback_count', 'overall_score', 'created_at'
+        ]
